a95bb9ef381b02cfcce42c537ed27520ab268cc9
rahulyewale <rahulsy20@gmail.com>
2016-08-30 09:26:35 +0530

    RY:initial Project check in

---
+++ portlets/globe-express-portlet/docroot/WEB-INF/src/com/express/globe/order/OrderDetails.java
@@ -0,0 +1,11 @@
+package com.express.globe.order;
+
+import com.liferay.util.bridges.mvc.MVCPortlet;
+
+/**
+ * Portlet implementation class OrderDetails
+ */
+public class OrderDetails extends MVCPortlet {
+ 
+
+}
