2ce0264b158e33d871178a83dc91e390408a7f0b
rahulyewale <rahulsy20@gmail.com>
2016-08-31 09:41:54 +0530

    RY:Added order details service code

---
+++ portlets/globe-express-portlet/docroot/WEB-INF/src/com/express/globe/service/impl/OrderDetailsLocalServiceImpl.java
@@ -0,0 +1,40 @@
+/**
+ * Copyright (c) 2000-2013 Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.express.globe.service.impl;
+
+import com.express.globe.service.base.OrderDetailsLocalServiceBaseImpl;
+
+/**
+ * The implementation of the order details local service.
+ *
+ * <p>
+ * All custom service methods should be put in this class. Whenever methods are added, rerun ServiceBuilder to copy their definitions into the {@link com.express.globe.service.OrderDetailsLocalService} interface.
+ *
+ * <p>
+ * This is a local service. Methods of this service will not have security checks based on the propagated JAAS credentials because this service can only be accessed from within the same VM.
+ * </p>
+ *
+ * @author rahuly
+ * @see com.express.globe.service.base.OrderDetailsLocalServiceBaseImpl
+ * @see com.express.globe.service.OrderDetailsLocalServiceUtil
+ */
+public class OrderDetailsLocalServiceImpl
+	extends OrderDetailsLocalServiceBaseImpl {
+	/*
+	 * NOTE FOR DEVELOPERS:
+	 *
+	 * Never reference this interface directly. Always use {@link com.express.globe.service.OrderDetailsLocalServiceUtil} to access the order details local service.
+	 */
+}
